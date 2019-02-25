914be4c1618029a72a4a54ae3694f7a34670e43f
Knut Zuidema <knut.zuidema@gmail.com>
2019-02-25 15:51:58 +0100

    add error message in blockchain constructor test

--- pkg/model/blockchain_test.go
+++ pkg/model/blockchain_test.go
@@ -12,15 +12,16 @@ func TestNewBlockchain(t *testing.T) {
 			t.Error("no genesis block")
 		}
 		if !bytes.Equal(got.Blocks[0].Data, []byte("Genesis Block")) {
-			t.Error("")
+			t.Errorf("did not set data of genesis block correctly, got %v, want %v", got.Blocks[0].Data,
+				[]byte("Genesis Block"))
 		}
 	})
 }
 
 func TestBlockchain_AddBlock(t *testing.T) {
 	tests := []struct {
-		name   string
-		args   []byte
+		name string
+		args []byte
 	}{
 		{"first", []byte("0")},
 		{"second", []byte("1")},
@@ -31,10 +32,10 @@ func TestBlockchain_AddBlock(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			beforeSize := len(chain.Blocks)
 			chain.AddBlock(tt.args)
-			if !(len(chain.Blocks) == beforeSize+ 1){
+			if !(len(chain.Blocks) == beforeSize+1) {
 				t.Error("did not add element to chain")
 			}
-			if !(bytes.Equal(chain.Blocks[index].Hash, chain.Blocks[index + 1].PreviousHash)){
+			if !(bytes.Equal(chain.Blocks[index].Hash, chain.Blocks[index+1].PreviousHash)) {
 				t.Error("hashes do not match")
 			}
 		})
