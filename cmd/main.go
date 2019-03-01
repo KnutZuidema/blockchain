9826bfcd67e4c5cf19554053c223dcaa25df8c97
Knut Zuidema <knut.zuidema@gmail.com>
2019-03-01 16:34:57 +0100

    add command line interface
    blocks may be added to a persistent chain and the chain can be
    printed to the command line

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"blockchain/pkg/models"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var data string
+var add = flag.NewFlagSet("add", flag.ExitOnError)
+
+func init() {
+	add.StringVar(&data, "data", "", "data to be stored in the block")
+}
+
+func main() {
+	chain, err := models.BlockchainFromDb("chain", "chain.db")
+	if err != nil {
+		fmt.Println("Couldn't open database")
+	}
+	switch os.Args[1] {
+	case "add":
+		if err := add.Parse(os.Args[2:]); err != nil {
+			flag.Usage()
+		}
+		chain.AddBlock([]byte(data))
+		err := chain.ToDb("chain", "chain.db")
+		if err != nil {
+			fmt.Println("Couldn't save data to db")
+		}
+	case "print":
+		fmt.Println(chain.String())
+	}
+}
