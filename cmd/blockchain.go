96d30d6334464455f9e1c365dd2a35bcfeae8ae0
Knut Zuidema <knut.zuidema@gmail.com>
2019-03-23 15:13:12 +0100

    add cli functionality for transaction based actions

--- cmd/blockchain.go
+++ cmd/blockchain.go
@@ -2,40 +2,54 @@ package main
 
 import (
 	"blockchain/pkg/models"
-	"flag"
 	"fmt"
+	"github.com/akamensky/argparse"
 	"os"
 )
 
-var data string
-var add = flag.NewFlagSet("add", flag.ExitOnError)
-
-func init() {
-	add.StringVar(&data, "data", "", "data to be stored in the block")
-}
-
 func main() {
-	chain, err := models.BlockchainFromDb("chain", "chain.db")
-	if err != nil {
-		fmt.Println("Couldn't open database")
+	create := argparse.NewParser("create", "create a blockchain")
+	creator := create.String("c", "creator", &argparse.Options{Required: true, Help: "creator of the chain"})
+	mine := argparse.NewParser("mine", "mine blocks")
+	receiver := mine.String("r", "receiver", &argparse.Options{Required: true, Help: "receiver of the reward"})
+	amount := mine.Int("a", "amount", &argparse.Options{Default: 1, Help: "amount of blocks to mine"})
+	if len(os.Args) < 2 {
+		fmt.Println("Need to specify an action")
+		return
 	}
 	switch os.Args[1] {
-	case "add":
-		if err := add.Parse(os.Args[2:]); err != nil {
-			add.Usage()
+	case "create":
+		if err := create.Parse(os.Args[1:]); err != nil {
+			fmt.Println(create.Usage(err))
 			return
 		}
-		if data == "" {
-			add.Usage()
-			return
-
-		}
-		chain.AddBlock([]byte(data))
-		err := chain.ToDb("chain", "chain.db")
+		chain, err := models.NewBlockchain(*creator)
 		if err != nil {
-			fmt.Println("Couldn't save data to db")
+			fmt.Println("Could not create blockchain")
+			return
+		}
+		fmt.Println(chain.String())
+	case "mine":
+		if err := mine.Parse(os.Args[1:]); err != nil {
+			fmt.Println(mine.Usage(err))
+			return
+		}
+		chain, err := models.GetBlockchain()
+		if err != nil {
+			fmt.Println("Could not open database")
+			return
+		}
+		for i := 0; i < *amount; i++ {
+			if err = chain.MineBlock(*receiver); err != nil {
+				fmt.Println("Couldn't mine block", err)
+			}
 		}
 	case "print":
+		chain, err := models.GetBlockchain()
+		if err != nil {
+			fmt.Println("Could not open database")
+			return
+		}
 		fmt.Println(chain.String())
 	}
 }
