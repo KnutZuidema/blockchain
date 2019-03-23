577eb3ecc3cbaec21dfb2649f26fd98b44b16fcc
Knut Zuidema <knut.zuidema@gmail.com>
2019-03-23 15:11:20 +0100

    use database directly, do not store blockchain in memory
    instead of providing functions to serialize the blockchain, only store the latest block in memory and work on the database directly when issuing any action on the blockchain

--- pkg/models/blockchain.go
+++ pkg/models/blockchain.go
@@ -2,40 +2,145 @@ package models
 
 import (
 	"encoding/json"
+	"errors"
 	bolt "go.etcd.io/bbolt"
+	"os"
 )
+
+var DbFilePath = "blockchain.db"
+var BucketName = "blocks"
+var LastBlockKey = "last"
 
 // Blockchain represents a chain of blocks. Its only member is a slice of blocks
 type Blockchain struct {
-	Blocks []*Block `json:"blocks"`
+	Tail     []byte   `json:"tail"`
+	Database *bolt.DB `json:"database"`
 }
 
 // NewBlockchain creates a new blockchain containing a genesis block. A genesis block is required, since every block
 // requires a previous block hash. The genesis block is the only block with a nil value for its previous hash
-func NewBlockchain() *Blockchain {
-	chain := &Blockchain{}
-	chain.Blocks = append(chain.Blocks, NewBlock([]*Transaction{NewGenesisTransaction("receiver", "genesis")}, nil))
-	return chain
-}
-
-func BlockchainFromDb(chainName, filepath string) (chain Blockchain, err error) {
-	db, err := bolt.Open(filepath, 0600, nil)
+func NewBlockchain(address string) (chain *Blockchain, err error) {
+	err = os.Remove(DbFilePath)
 	if err != nil {
 		return
 	}
-	defer db.Close()
+	db, err := bolt.Open(DbFilePath, 0600, nil)
+	if err != nil {
+		return
+	}
+	var tail []byte
 	err = db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte("chains"))
+		genesis := NewGenesisBlock([]*Transaction{NewGenesisTransaction(address)})
+		bucket, err := tx.CreateBucket([]byte(BucketName))
+		bytes, err := json.Marshal(genesis)
 		if err != nil {
 			return err
 		}
-		bytes := bucket.Get([]byte(chainName))
-		if bytes == nil {
-			chain = *NewBlockchain()
-		} else {
-			err := json.Unmarshal(bytes, &chain)
+		err = bucket.Put(genesis.Hash, bytes)
+		if err != nil {
+			return err
+		}
+		err = bucket.Put([]byte(LastBlockKey), genesis.Hash)
+		if err != nil {
+			_ = bucket.Delete(genesis.Hash)
+			return err
+		}
+		tail = genesis.Hash
+		return nil
+	})
+	if err != nil {
+		return
+	}
+	chain = &Blockchain{tail, db}
+	return
+}
+
+func GetBlockchain() (chain *Blockchain, err error) {
+	db, err := bolt.Open(DbFilePath, 0600, nil)
+	if err != nil {
+		return
+	}
+	var tail []byte
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BucketName))
+		if bucket == nil {
+			return errors.New("there is no existing blockchain")
+		}
+		tail = bucket.Get([]byte(LastBlockKey))
+		return nil
+	})
+	if err != nil {
+		return
+	}
+	chain = &Blockchain{tail, db}
+	return
+}
+
+// AddBlock adds a block with the specified data to the chain using the hash of the latest block in the chain as
+// previous block hash for the new block
+func (chain *Blockchain) AddBlock(transactions []*Transaction) (err error) {
+	block := NewBlock(transactions, chain.Tail)
+	bytes, err := json.Marshal(block)
+	if err != nil {
+		return
+	}
+	err = chain.Database.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BucketName))
+		err := bucket.Put(block.Hash, bytes)
+		if err != nil {
+			return err
+		}
+		err = bucket.Put([]byte(LastBlockKey), block.Hash)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return
+	}
+	chain.Tail = block.Hash
+	return
+}
+
+func (chain *Blockchain) MineBlock(address string) (err error) {
+	err = chain.AddBlock([]*Transaction{NewGenesisTransaction(address)})
+	return
+}
+
+func (chain Blockchain) End() (block Block, err error) {
+	return chain.Get(chain.Tail)
+}
+
+func (chain Blockchain) Get(hash []byte) (block Block, err error) {
+	err = chain.Database.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BucketName))
+		err := json.Unmarshal(bucket.Get(hash), block)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return
+}
+
+func (chain Blockchain) Range(action func(Block) bool) (err error) {
+	nextKey := chain.Tail
+	var block Block
+	err = chain.Database.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(BucketName))
+		for {
+			bytes := bucket.Get(nextKey)
+			if bytes == nil {
+				return nil
+			}
+			err := json.Unmarshal(bytes, block)
 			if err != nil {
 				return err
+			}
+			nextKey = block.PreviousHash
+			if !action(block) {
+				break
 			}
 		}
 		return nil
@@ -43,53 +148,10 @@ func BlockchainFromDb(chainName, filepath string) (chain Blockchain, err error) 
 	return
 }
 
-// AddBlock adds a block with the specified data to the chain using the hash of the latest block in the chain as
-// previous block hash for the new block
-func (chain *Blockchain) AddBlock(transactions []*Transaction) {
-	block := NewBlock(transactions, chain.Blocks[len(chain.Blocks)-1].Hash)
-	if !NewProofOfWork(block).Validate() {
-		return
-	}
-	chain.Blocks = append(chain.Blocks, block)
-}
-
-func (chain Blockchain) ToDb(chainName, filepath string) error {
-	bytes, err := json.Marshal(chain)
-	if err != nil {
-		return err
-	}
-	db, err := bolt.Open(filepath, 0600, nil)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	err = db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte("chains"))
-		if err != nil {
-			return err
-		}
-		err = bucket.Put([]byte(chainName), bytes)
-		if err != nil {
-			return err
-		}
-		return nil
+func (chain Blockchain) String() (str string) {
+	_ = chain.Range(func(block Block) bool {
+		str = block.String() + "\n" + str
+		return true
 	})
-	return nil
-}
-
-func (chain Blockchain) String() string {
-	str := ""
-	for _, block := range chain.Blocks {
-		str += block.String() + "\n"
-	}
-	return str
-}
-
-func (chain Blockchain) Equals(otherChain Blockchain) bool {
-	for index := range chain.Blocks {
-		if !chain.Blocks[index].Equals(*otherChain.Blocks[index]) {
-			return false
-		}
-	}
-	return true
+	return
 }
